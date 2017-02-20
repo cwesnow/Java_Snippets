bac1946cd5d6b9396dbeb2ad0b1db58601c5341d
Wesley Cox <cwesnow2000@yahoo.com>
2017-02-20 00:08:18 -0600

    Update MonsterSim.java

--- MonsterSim.java
+++ MonsterSim.java
@@ -12,18 +12,18 @@ public class MonsterSim
     {
     	round++;
     	System.out.println("\nRound " + round);
-       // Print result of the attack method
-      System.out.println( monster1.attack(monster2) );
-      System.out.println( monster2.attack(monster1) );
+
+      	// Print result of the attack method
+    	System.out.println( monster1.attack(monster2) );
+    	System.out.println( monster2.attack(monster1) );
     }
     System.out.print("\nEnd Combat: ");
-    if( monster1.health < 1 && monster2.health < 1)
-    	System.out.printf("The %s and the %s died fighting each other!", monster1.name,monster2.name);
-	else if( monster1.health > 0 ) {
-      System.out.printf("%s wins!", monster1.name);
-    }
-    else {
-      System.out.printf("%s wins!", monster2.name);
+    if( monster1.health < 1 && monster2.health < 1) {
+	    System.out.printf("The %s and the %s died fighting each other!", monster1.name,monster2.name);
+    } else if ( monster1.health > 0 ) {
+	    System.out.printf("%s wins!", monster1.name);
+	} else {
+	    System.out.printf("%s wins!", monster2.name);
     }  
   }
 }
