88e679ca7232a686669453d1750a7708065c19f1
Wesley Cox <cwesnow2000@yahoo.com>
2017-02-19 23:48:40 -0600

    Create BiggestSmallest.java

---
+++ BiggestSmallest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Program
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in); 
+    int numbers[] = new int[10];
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		
+		for (int x = 0; x < numbers.length; x++) {
+    		System.out.print("Enter a number: ");
+    		numbers[x] = scan.nextInt();
+    		if( numbers[x] < min) min = numbers[x];
+    		if( numbers[x] > max) max = numbers[x];
+		}
+		System.out.printf("Max: %d Min: %d", max, min);
+	}
+}
